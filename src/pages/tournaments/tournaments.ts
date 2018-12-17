c476ca523d359710e8be4746b9bb271f3d6c2eaa
Dalton Whyte <daltino06@yahoo.com>
2018-12-17 12:21:18 +0100

    First commit, created the following pages: TeamHomePage MyTeamsPage TeamDetailPage StandingsPage TournamentPage
    Also added a tabbed view in the TeamHomePage which contains the TeamDetailPage and StandingsPage as tabs.

---
+++ src/pages/tournaments/tournaments.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MyTeamsPage } from '../my-teams/my-teams';
+import { TeamsPage } from '../teams/teams';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-tournaments',
+  templateUrl: 'tournaments.html',
+})
+export class TournamentsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TournamentsPage');
+  }
+
+  itemTapped() {
+    // this.navCtrl.push(MyTeamsPage);
+    this.navCtrl.push(TeamsPage);
+  }
+
+}
